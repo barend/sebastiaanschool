7fa0823cc1f5a56a9f0246456459caaadcd6b73c
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-01-11 21:01:43 +0100

    Applied nice color.

---
+++ SebastiaanSchool/Classes/SBSStyle.h
@@ -0,0 +1,15 @@
+//
+//  SBSStyle.h
+//  SebastiaanSchool
+//
+//  Created by Jeroen Leenarts on 11-01-13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBSStyle : NSObject
+
++ (UIColor *)sebastiaanBlueColor;
+
+@end
---
+++ SebastiaanSchool/Classes/SBSStyle.m
@@ -0,0 +1,25 @@
+//
+//  SBSStyle.m
+//  SebastiaanSchool
+//
+//  Created by Jeroen Leenarts on 11-01-13.
+//
+//
+
+#import "SBSStyle.h"
+
+static UIColor * sebastiaanBlueColor;
+
+@implementation SBSStyle
+
++ (void)initialize {
+    if ([SBSStyle class] == self) {
+        sebastiaanBlueColor = [UIColor colorWithRed:0.0f green:0.67058823499999998f blue:0.90196078400000002f alpha:1.0];
+    }
+}
+
++ (UIColor *)sebastiaanBlueColor {
+    return sebastiaanBlueColor;
+}
+
+@end
