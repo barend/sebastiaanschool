77b1d82b1176da608cf2127405832a33cfa4fd6d
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-02-09 19:37:27 +0100

    Added icon and background color to info VC.

--- SebastiaanSchool/Classes/SBSInfoViewController.m
+++ SebastiaanSchool/Classes/SBSInfoViewController.m
@@ -30,7 +30,7 @@
 {
     [super viewDidLoad];
     [TestFlight passCheckpoint:[NSString stringWithFormat:@"Loaded VC %@", self.title]];
-    
+    self.view.backgroundColor = [SBSStyle sebastiaanBlueColor];
     [self applyTitle:NSLocalizedString(@"Call", nil) andWithImageNamed:@"75-phone" toButton:self.callButton];
     [self applyTitle:NSLocalizedString(@"About", nil) andWithImageNamed:@"123-id-card" toButton:self.aboutButton];
     [self applyTitle:NSLocalizedString(@"Agenda", nil) andWithImageNamed:@"259-list" toButton:self.agendaButton];
