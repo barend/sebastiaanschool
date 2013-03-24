c20e3a98d43c16c76e56e27c45d5a96cc5805242
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-03-24 11:36:28 +0100

    Implemented data classes with dynamic properties.

---
+++ SebastiaanSchool/Classes/SBSAgendaItem.h
@@ -0,0 +1,18 @@
+//
+//  SBSAgendaItem.h
+//  SebastiaanSchool
+//
+//  Created by Jeroen Leenarts on 24-03-13.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface SBSAgendaItem : PFObject<PFSubclassing>
+
+@property (retain) NSString *name;
+@property (retain) NSString *type;
+@property (retain) NSDate *start;
+@property (retain) NSDate *end;
+
+@end
